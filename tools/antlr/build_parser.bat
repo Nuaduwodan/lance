d91d7d3504aaa0b83d51de2929fede26cc691187
Yanik Bigler <yanik.bigler@gmail.com>
2023-03-08 17:20:26 +0100

    add visitor interface to generator

--- tools/antlr/build_parser.bat
+++ tools/antlr/build_parser.bat
@@ -1,2 +1,2 @@
-java -jar antlr-4.11.1-complete.jar -Dlanguage=CSharp ..\..\antlr4-grammar\SinumerikNC.g4
+java -jar antlr-4.11.1-complete.jar -visitor -Dlanguage=CSharp ..\..\antlr4-grammar\SinumerikNC.g4
 move ..\..\antlr4-grammar\*.cs ..\..\lance-server\Parser\
