d40b71a70dec921834594616761f580ec1562cfd
Yanik Bigler <yanik.bigler@gmail.com>
2023-03-08 17:21:13 +0100

    change test framework to ms test

---
+++ LanceServerTest/Workspace/UtilTest.cs
@@ -0,0 +1,20 @@
+﻿using LanceServer.Core.Workspace;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LanceServerTest
+{
+    [TestClass]
+    public class UtilTest
+    {
+    
+        [TestMethod]
+        public void EmptyData()
+        {
+            var expectedResult = new Document(new Uri("c:/Users/YBigler/workspace/lance/LanceServerTest/InCycleFpController.spf"));
+        
+            var actualResult = Util.ReadDocument(new Uri(Uri.UnescapeDataString("file:///c%3A/Users/YBigler/workspace/lance/LanceServerTest/InCycleFpController.spf")));
+
+            Assert.AreEqual(expectedResult.Uri, actualResult.Uri);
+        }
+    }
+}
