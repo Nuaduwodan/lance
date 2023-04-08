2ec93fe47b1f41ad2f7e9fd9d45eb85065ad406f
Yanik Bigler <yanik.bigler@gmail.com>
2023-04-08 21:36:10 +0200

    removes hosting again

--- lance-server/Program.cs
+++ lance-server/Program.cs
@@ -5,8 +5,6 @@ using LanceServer.Hover;
 using LanceServer.Parser;
 using LanceServer.Preprocessor;
 using LanceServer.SemanticToken;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace LanceServer
 {
@@ -16,49 +14,24 @@ namespace LanceServer
 
         private static async Task MainAsync(string[] args)
         {
-            /*var builder = Host.CreateDefaultBuilder(args);
-
-            builder.ConfigureServices(services =>
-                services.AddSingleton(LspServerFactory)
-                    .AddSingleton<IConfigurationManager, ConfigurationManager>()
-                    .AddSingleton<ICustomPreprocessor, CustomPreprocessor>()
-                    .AddSingleton<IWorkspace, Workspace>()
-                    .AddSingleton<IParserManager, ParserManager>()
-                    .AddSingleton<IHoverHandler, HoverHandler>()
-            );
-
-            using var host = builder.Build();
-
-            host.Run();
-            */
             Stream stdin = new StreamSplitter(Console.OpenStandardInput(), new StreamLog("editor"), StreamSplitter.StreamOwnership.OwnNone);
             Stream stdout = new StreamSplitter(Console.OpenStandardOutput(), new StreamLog("server"), StreamSplitter.StreamOwnership.OwnNone);
             var config = new ConfigurationManager();
+            var parser = new ParserManager();
+            var customPreprocessor = new CustomPreprocessor(config);
+            var workspace = new Workspace(parser, customPreprocessor, config);
+            var semanticTokenHandler = new SemanticTokenHandler();
+            var hoverHandler = new HoverHandler(config);
             var lsp = new LSPServer(
                 stdout,
                 stdin,
-                new Workspace(new ParserManager(), new CustomPreprocessor(config),config),
+                workspace,
                 config,
-                new SemanticTokenHandler(),
-                new HoverHandler(config)
+                semanticTokenHandler,
+                hoverHandler
             );
             
             await Task.Delay(-1);
-        }
-
-        private static LSPServer LspServerFactory(IServiceProvider provider)
-        {
-            Stream stdin = new StreamSplitter(Console.OpenStandardInput(), new StreamLog("editor"), StreamSplitter.StreamOwnership.OwnNone);
-            Stream stdout = new StreamSplitter(Console.OpenStandardOutput(), new StreamLog("server"), StreamSplitter.StreamOwnership.OwnNone);
-
-            return new LSPServer(
-                stdout,
-                stdin,
-                provider.GetService<IWorkspace>()!,
-                provider.GetService<IConfigurationManager>()!,
-                provider.GetService<ISemanticTokenHandler>()!,
-                provider.GetService<IHoverHandler>()!
-            );
         }
     }
 }
