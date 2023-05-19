34de7a34572bc0ba81bbc690db9ceda21bc68a4e
Yanik Bigler <yanik.bigler@gmail.com>
2023-05-19 13:20:51 +0200

    adds documentation

--- LanceServer/Core/Symbol/SymbolTable.cs
+++ LanceServer/Core/Symbol/SymbolTable.cs
@@ -9,16 +9,27 @@ public class SymbolTable
 {
     private Dictionary<string, AbstractSymbol> _symbols = new();
 
-    public bool AddSymbol(AbstractSymbol symbol)
+    /// <summary>
+    /// Attempts to add the symbol to the table.
+    /// </summary>
+    /// <returns>True if the symbol was added successfully, false otherwise.</returns>
+    public bool TryAddSymbol(AbstractSymbol symbol)
     {
         return _symbols.TryAdd(symbol.Identifier.ToLower(), symbol);
     }
 
+    /// <summary>
+    /// Tries to get the symbol referenced by the symbol name.
+    /// </summary>
+    /// <returns>True if the symbol was found, false otherwise.</returns>
     public bool TryGetSymbol(string symbolName, [MaybeNullWhen(false)] out AbstractSymbol symbol)
     {
         return _symbols.TryGetValue(symbolName.ToLower(), out symbol);
     }
 
+    /// <summary>
+    /// Gets all symbols in this symbol table.
+    /// </summary>
     public IEnumerable<AbstractSymbol> GetAll()
     {
         return _symbols.Select(pair => pair.Value);
