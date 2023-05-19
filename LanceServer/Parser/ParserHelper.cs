1f90ea303e2292a3096f094499d2a5b2775cdcef
Yanik Bigler <yanik.bigler@gmail.com>
2023-05-19 09:22:47 +0200

    revert wrong character offset

--- LanceServer/Parser/ParserHelper.cs
+++ LanceServer/Parser/ParserHelper.cs
@@ -9,16 +9,16 @@ public static class ParserHelper
     public static Range GetRangeBetweenTokens(IToken startToken, IToken endToken)
     {
         var lineStart = (uint)startToken.Line - 1;
-        var characterStart = (uint)startToken.Column - 1;
+        var characterStart = (uint)startToken.Column;
         var lineEnd = (uint)endToken.Line - 1;
-        var characterEnd = (uint)endToken.Column + (uint)endToken.Text.Length - 1;
+        var characterEnd = (uint)endToken.Column + (uint)endToken.Text.Length;
         return new Range { Start = new Position(lineStart, characterStart), End = new Position(lineEnd, characterEnd) };
     }
 
     public static Range GetRangeForToken(IToken identifierToken)
     {
         var line = (uint)identifierToken.Line - 1;
-        var characterStart = (uint)identifierToken.Column - 1;
+        var characterStart = (uint)identifierToken.Column;
         var characterEnd = characterStart + (uint)identifierToken.Text.Length;
         return new Range { Start = new Position(line, characterStart), End = new Position(line, characterEnd) };
     }
