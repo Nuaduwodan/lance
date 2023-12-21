f71dbeea7611cf7f081f3fed1ffa8d019555c39e
Yanik Bigler <yanik.bigler@gmail.com>
2023-12-21 17:52:03 +0100

    improves build scripts

--- lance-extension/build_release_server.bat
+++ lance-extension/build_release_server.bat
@@ -3,6 +3,7 @@ dotnet clean -c release
 dotnet build -c release
 popd
 xcopy ..\LanceServer\bin\Release\net6.0 server\net6.0\ /s /y
-cd server\net6.0
+pushd server\net6.0
 del config.json
 del preprocessor_config.json
+popd
